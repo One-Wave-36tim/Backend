2359955a703470a0c1ff1e409d267972c0160644
Jimin Park <parkjm012@naver.com>
2026-02-07 00:01:46 +0900

    feat: add Portfolio and UserSettings entities with relationships to User

---
+++ app/db/entities/__init__.py
@@ -0,0 +1,5 @@
+from app.db.entities.portfolio import Portfolio
+from app.db.entities.user import User
+from app.db.entities.user_settings import UserSettings
+
+__all__ = ["Portfolio", "User", "UserSettings"]
